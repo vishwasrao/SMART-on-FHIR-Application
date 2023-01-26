10776c45b94a0b8a77b127f4457441ece7f8dab4
Vishwasrao Salunkhe <vishwasrao.salunkhe@gmail.com>
2023-01-26 09:19:27 -0500

    fixing unit test

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
